8165e54b4927cf2057997797c358589473547865
ityukifukunaga-cyber <it.yuki.fukunaga@gmail.com>
2026-01-19 23:41:21 +0900

    Refactor todo insert and list into functions

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from db.connection import get_connecction
 
-def main():
+def add_todo(title):
   conn = get_connecction()
   cursor = conn.cursor()
 
   sql = "INSERT INTO todos (title) VALUES (%s)"
-  cursor.execute(sql, ("PythonでTODO作成",))
+  cursor.execute(sql, (title,))
 
   conn.commit()
-
-  print("TODO inserted")
 
   cursor.close()
   conn.close()
 
+def show_todos():
+  conn = get_connecction()
+  cursor = conn.cursor()
+
+  sql ="SELECT id, title, is_completed, created_at FROM todos"
+  cursor.execute(sql)
+
+  todos = cursor.fetchall()
+
+  print("==== TODO LIST ====")
+  for todo in todos:
+    status = "✓" if todo[2] else ""
+    print(f"[{status}] {todo[0]}: {todo[1]}")
+
+  cursor.close()
+  conn.close()
+
+def main():
+  add_todo("関数の変化")
+  show_todos()
 
 if __name__ == "__main__":
   main()
